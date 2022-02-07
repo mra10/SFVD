d08bb43144915fb0922ff78ac115c4f021447c5f
Mushfiqur Rahman <deathman.mushfiqur@gmail.com>
2022-02-07 11:46:31 +0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Class1
+{
+    public static async void getMethodAsync()
+    {
+        using (HttpClient httpclient = new HttpClient())
+        {
+            // var request = new HttpRequestMessage(HttpMethod.Get, url); ;
+
+            var productValue = new ProductInfoHeaderValue("ScraperBot", "1.0");
+            var commentValue = new ProductInfoHeaderValue("(+http://www.example.com/ScraperBot.html)");
+
+            httpclient.DefaultRequestHeaders.UserAgent.Add(productValue);
+            httpclient.DefaultRequestHeaders.UserAgent.Add(commentValue);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://API.com/api");
+            using (var resp = await httpclient.SendAsync(request))
+            {
+                //  HttpResponseMessage responce = await httpclient.SendAsync(request);
+
+                Console.WriteLine("after");
+
+                var header = resp.Headers;
+                string s = header.ToString();
+                StreamWriter sw = new StreamWriter("1".ToString() + "-header.txt");
+                sw.WriteLine(s);
+                sw.Close();
+            }
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public Class1()
+	{
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ WebRequest/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebRequest
+{
+    internal class Program
+    {
+        public static string url;
+        public static int count = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a URL");
+            url = Console.ReadLine();
+          
+          Console.WriteLine(url);
+            Thread t = new Thread(getMethod);
+            Thread t1 = new Thread(getMethod);
+            t1.Start();
+            t.Start();
+            t.Join();
+            
+            t1.Join();
+            Console.WriteLine("hello");
+            Console.ReadLine();
+            
+           
+        }
+
+        
+
+
+
+
+        public static async void getMethod()
+        {
+            using (HttpClient httpclient = new HttpClient())
+            {
+
+                using (HttpResponseMessage responce = await httpclient.GetAsync(Program.url))
+                {
+                    
+                        var header = responce.Headers;
+                        string s = header.ToString();
+                        StreamWriter sw = new StreamWriter(Program.count.ToString() + "-header.txt");
+                        count++;
+                        sw.WriteLine(s);
+                    Console.WriteLine(s);
+                        sw.Close();
+
+
+                    
+
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
