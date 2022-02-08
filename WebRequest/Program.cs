8247d30e615c9595d6847e4b88e6b7c2e875d060
Mushfiqur Rahman <deathman.mushfiqur@gmail.com>
2022-02-09 01:30:25 +0600

    seassion extracted

--- WebRequest/Program.cs
+++ WebRequest/Program.cs
@@ -14,20 +14,32 @@ namespace WebRequest
     {
         public static string url;
         public static int count = 1;
+        public static string[] coocie = new string[3];
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a URL");
-            url = Console.ReadLine();
-          
+            url = "https://www.amazon.com";
+
+
           Console.WriteLine(url);
             Thread t = new Thread(getMethod);
             Thread t1 = new Thread(getMethod);
             t1.Start();
             t.Start();
             t.Join();
-            
             t1.Join();
+
+
             Console.WriteLine("hello");
+            //count = 1;
+            //Thread t2 = new Thread(coocieExtractor);
+            //Thread t3 = new Thread(coocieExtractor);
+            //t2.Start();
+            //t3.Start();
+            //t2.Join();
+            //t3.Join();
+            //Console.WriteLine("\n\n\n"+coocie[1]);
+            //Console.WriteLine("\n\n\n" +coocie[2]);
             Console.ReadLine();
             
            
@@ -51,15 +63,50 @@ namespace WebRequest
                         StreamWriter sw = new StreamWriter(Program.count.ToString() + "-header.txt");
                         count++;
                         sw.WriteLine(s);
-                        Console.WriteLine(s);
+                        //Console.WriteLine(s);
                         sw.Close();
+                        string[] pieces = s.Split(' ');
+                    int counter = 0;
+                    while (pieces[counter] != null)
+                    {
+                        //Console.WriteLine("dhukse");
 
+                        if (pieces[counter].Contains("ookie")) {
+
+                            Program.coocie[count -1]= pieces[counter+1];
+                            Console.WriteLine("\n\n\n" + pieces[counter + 1]);
+                            break;
+
+
+                        }
+
+                        counter++;
+                    }
 
                     
 
                 }
 
             }
+        }
+        public static void coocieExtractor() {
+
+            StreamReader reader = new StreamReader(Program.count.ToString() + "-header.txt");
+            int inCount = count;
+            count++;
+            while (!reader.EndOfStream)
+            {   
+                string line = reader.ReadLine();    
+
+                if (line.Contains("oocie"))
+                {
+
+                    Program.coocie[inCount] = reader.ReadLine();
+                    break;
+                }
+
+            }
+        
         }
 
 
