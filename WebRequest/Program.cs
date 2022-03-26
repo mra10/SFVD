cf5f185f3d23c4063bc387c738d49906006c2b28
Mushfiqur Rahman <deathman.mushfiqur@gmail.com>
2022-03-27 01:20:58 +0600

    before Auth compleate & extract information from form compleate

--- WebRequest/Program.cs
+++ WebRequest/Program.cs
@@ -18,7 +18,7 @@ namespace WebRequest
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a URL");
-            url = "https://www.udemy.com";
+            url = "http://192.168.0.108/ESAPI-Java-SwingSet-Interactive/main?function=InsecureLogin&lab";
 
 
           Console.WriteLine(url);
@@ -30,7 +30,7 @@ namespace WebRequest
             t1.Join();
 
 
-            Console.WriteLine("hello");
+          //  Console.WriteLine("hello");
             //count = 1;
             //Thread t2 = new Thread(coocieExtractor);
             //Thread t3 = new Thread(coocieExtractor);
@@ -41,8 +41,21 @@ namespace WebRequest
             //Console.WriteLine("\n\n\n"+coocie[1]);
             //Console.WriteLine("\n\n\n" +coocie[2]);
             Console.ReadLine();
+            if (coocie[0] != coocie[1])
+            {
+
+
+                Console.WriteLine("before auth Secure");
+
+            }
+            else {
+                Console.WriteLine("before seassion matched : unsafe");
             
-           
+            }
+            Console.ReadLine();
+            extractor();
+
+            Console.ReadLine();
         }
 
         
@@ -55,31 +68,47 @@ namespace WebRequest
             using (HttpClient httpclient = new HttpClient())
             {
                 httpclient.DefaultRequestHeaders.Add("User-Agent", "C# App");
+                Console.WriteLine(httpclient); 
 
                 using (HttpResponseMessage responce = await httpclient.GetAsync(Program.url))
                 {
                     
                         var header = responce.Headers;
-                        string s = header.ToString();
-                        StreamWriter sw = new StreamWriter(Program.count.ToString() + "-header.txt");
-                        count++;
-                        sw.WriteLine(s);
-                        Console.WriteLine(s);
+                        using (HttpContent content = responce.Content)
+                        {
+                        string text = await content.ReadAsStringAsync();
+
+                        StreamWriter sw = new StreamWriter(count.ToString() + "-content.txt");
+                        sw.WriteLine(text);
+                        Console.WriteLine(text);
                         sw.Close();
+
+                         }
+
+                    string s = header.ToString();
+                        StreamWriter headerWriter = new StreamWriter(count.ToString() + "-header.txt");
+                        count++;
+                        headerWriter.WriteLine(s);
+                       //Console.WriteLine(s);
+                        headerWriter.Close();
+
+                    StreamWriter bodyWriter = new StreamWriter(count.ToString() + "-body.txt");
+                     
+                    bodyWriter.Close(); 
                         string[] pieces = s.Split(' ');
-                    int counter = 0;
-                    while (pieces.Length>=counter)
-                    {
-                        //Console.WriteLine("dhukse");
+                         int counter = 0;
+                         while (pieces.Length>=counter)
+                         {
+                                
 
-                        if (pieces[counter].Contains("Set-Cookie")) {
+                             if (pieces[counter].Contains("Set-Cookie")) {
 
-                            Program.coocie[count -1]= pieces[counter+1];
+                            coocie[count -1]= pieces[counter+1];
                             Console.WriteLine("\n\n\n" + pieces[counter + 1]);
                             break;
 
 
-                        }
+                         }
 
                         counter++;
                     }
@@ -90,6 +119,12 @@ namespace WebRequest
 
             }
         }
+
+
+
+
+
+
         public static void coocieExtractor() {
 
             StreamReader reader = new StreamReader(Program.count.ToString() + "-header.txt");
@@ -108,6 +143,130 @@ namespace WebRequest
 
             }
         
+        }
+
+
+
+
+        public static string purifier(string value)
+        {
+
+
+            int start = 0;
+            string a = "";
+            char[] b = value.ToCharArray();
+            foreach (char c in b)
+            {
+
+                if (c == '"')
+                {
+                    if (start == 0)
+                    {
+                        start++;
+                    }
+                    else
+                    {
+                        //Console.WriteLine(co);
+                        return a;
+                    }
+
+
+                }
+                else
+                {
+
+                    if (start > 0)
+                    {
+                        a = a + c.ToString();
+
+                    }
+
+                }
+
+            }
+
+
+            return "dsad";
+        }
+        public static string extractor()
+        {
+
+            StreamReader reader = new StreamReader("1-content.txt");
+            string action;
+            string[] user = new string[4];
+            int count = 0;
+            while (!reader.EndOfStream)
+            {
+                string tester = reader.ReadLine();
+                if (tester.Contains("form"))
+                {
+                    string[] pieces = tester.Split(' ');
+                    foreach (string prs in pieces)
+                    {
+                        if (prs.Contains("action"))
+                        {
+
+                            //int end = prs.Length;
+                            //end -= 2;
+                            //string ac = prs.Substring(8);
+                            //action = ac.Remove(ac.Length - 1);
+                            //Console.WriteLine(action);
+                            Console.WriteLine(purifier(prs));
+                            break;
+                        }
+
+                    }
+
+
+                }
+                if (tester.Contains("input"))
+                {
+                    string[] pieces = tester.Split(' ');
+                    foreach (string prs in pieces)
+                    {
+                        if (prs.Contains("name="))
+                        {
+
+                            //int end = prs.Length;
+                            //end -= 2;
+                            //string ac = prs.Substring(6);
+                            //user[count]= ac.Remove(ac.Length - 1);
+                            //Console.WriteLine(user[count]);
+                            Console.WriteLine(purifier(prs));
+                            count++;
+
+
+
+                        }
+                        if (prs.Contains("value") && count == 2)
+                        {
+                            //Console.WriteLine(prs);
+                            //int end = prs.Length;
+                            //end -= 2;
+                            //string ac = prs.Substring(7);
+                            //user[count] = ac.Remove(ac.Length - 1);
+
+                            //if (user[count].Contains("\"")){
+                            //    user[count] = user[count].Remove(user[count].Length - 1);
+                            //}
+
+
+
+
+                            //Console.WriteLine(user[count]);
+                            Console.WriteLine(purifier(prs));
+                            break;
+                        }
+
+
+                    }
+
+
+                }
+            }
+
+
+            return "not implimented";
         }
 
 
